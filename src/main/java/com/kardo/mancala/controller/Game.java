055fd56db43f8dfa644a95b6b00a84869e73790f
Kardelen Hatun-Oguz <kardelen.hatun-oguz@nedap.com>
2014-12-13 13:09:14 +0100

    initial board is drawn

--- src/main/java/com/kardo/mancala/controller/Game.java
+++ src/main/java/com/kardo/mancala/controller/Game.java
@@ -1,18 +1,19 @@
 package com.kardo.mancala.controller;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 import com.kardo.mancala.model.Bowl;
 import com.kardo.mancala.model.GravaHal;
 
 public class Game {
-	LinkedList<Bowl> board;
+	ArrayList<Bowl> board;
 	private final int player1 = 1;
 	private final int player2 = 2;
 	private final int initalNrOfSeeds = 6;
 
 	public Game() {
-		board = new LinkedList<>();
+		board = new ArrayList<>();
 		initForPlayer(player1);
 		initForPlayer(player2);
 	}
@@ -21,7 +22,11 @@ public class Game {
 		for(int i = 0; i < 6; i++) {
 			board.add(new Bowl(initalNrOfSeeds, playerId));
 		}
-		board.add(new GravaHal(initalNrOfSeeds, playerId));
+		board.add(new GravaHal(0 , playerId));
+	}
+	
+	public ArrayList<Bowl> getBoard() {
+		return board;
 	}
 	
 	
--- src/main/java/com/kardo/mancala/view/Board.java
+++ src/main/java/com/kardo/mancala/view/Board.java
@@ -1,16 +1,23 @@
 package com.kardo.mancala.view;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
+import java.awt.RenderingHints;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JApplet;
 import javax.swing.JPanel;
 import javax.swing.border.BevelBorder;
+
+import com.kardo.mancala.controller.Game;
+import com.kardo.mancala.model.Bowl;
 
 public class Board extends JApplet {
 
@@ -21,12 +28,15 @@ public class Board extends JApplet {
 	JPanel bowls;
 	Container contentPane;
 	final int width = 100;
+	Game game;
+	ArrayList<JPanel> boardGUI;
 
 	@Override
 	public void init() {
 
 		this.setSize(width*8, width*2);
 		
+		boardGUI = new ArrayList<>();
 		contentPane = new JPanel();
 		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
 		grava2 = new JPanel();
@@ -40,8 +50,13 @@ public class Board extends JApplet {
 		bowls = new JPanel();
 		bowls.setLayout(new BoxLayout(bowls, BoxLayout.Y_AXIS));
 
+		game = new Game();
+		
 		createBowls(userBowls1);
+		boardGUI.add(grava1);
 		createBowls(userBowls2);
+		boardGUI.add(grava2);
+		createSeeds();
 		
 		bowls.add(userBowls1);
 		bowls.add(userBowls2);
@@ -59,9 +74,50 @@ public class Board extends JApplet {
 		for(int i = 0; i < 6; i++) {
 			JPanel bowl = new JPanel();
 			bowl.setPreferredSize(new Dimension(width, width));
+			bowl.setLayout(new FlowLayout(FlowLayout.CENTER));
 			bowl.setBackground(Color.yellow);
 			bowl.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED, Color.lightGray, Color.GRAY));
 			userBowls.add(bowl);
+			boardGUI.add(bowl);
 		}
 	}
+	
+	public void createSeeds() {
+		ArrayList<Bowl> board = game.getBoard();
+		for(int i = 0; i < board.size(); i++) {
+			int seeds = board.get(i).getSeeds();
+			createSeeds(boardGUI.get(i), seeds);		
+		}
+		
+	}
+
+	private void createSeeds(JPanel bowl, int seeds) {
+		for(int i = 0; i < seeds; i++){
+			bowl.add(new Seed());
+		}		
+	}
+	
+	
+	
+	class Seed extends JPanel {
+	    @Override
+	    public void paintComponent(Graphics g) {
+	    	super.paintComponent(g);
+	    	Graphics2D g2d = (Graphics2D) g;
+	        g2d.setColor(Color.RED);
+	        g2d.fillOval(0, 0, 20, 20);
+	    }
+	    
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(20, 20);
+        }
+        
+        @Override
+        public Color getBackground() {
+        	return Color.yellow;
+        }
+
+	}
+	
 }
